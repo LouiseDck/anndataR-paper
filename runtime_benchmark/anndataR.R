649a81389b402a651a937ada14de9d9ec306cb49
LouiseDck <louise.deconinck@gmail.com>
2026-01-23 18:37:43 +0100

    Adds runtime benchmark code

---
+++ runtime_benchmark/anndataR.R
@@ -0,0 +1,88 @@
+library(anndataR)
+
+filenames <- c(
+    "runtime_benchmark/data/d100.h5ad",
+    "runtime_benchmark/data/d1000.h5ad",
+    "runtime_benchmark/data/d10000.h5ad",
+    "runtime_benchmark/data/d100000.h5ad",
+    "runtime_benchmark/data/d500000.h5ad"
+)
+
+rownames <- c("d100", "d1000", "d10000", "d100000", "d500000")
+
+times_adR <- lapply(filenames, function(fn) {
+    print(paste0("Started file ", fn))
+    system.time(ad <- anndataR::read_h5ad(fn))
+})
+user_adR <- sapply(times_adR, function(t) t[[1]])
+elapsed_adR <- sapply(times_adR, function(t) t[[3]])
+
+adR_user_elapsed <- data.frame(
+    user = user_adR,
+    elapsed = elapsed_adR,
+    row.names = rownames
+)
+
+library(zellkonverter)
+
+times_zk <- lapply(filenames, function(fn) {
+    print(paste0("Started file ", fn))
+    system.time(ad <- zellkonverter::readH5AD(fn, reader = "R"))
+})
+
+user_zk <- sapply(times_zk, function(t) t[[1]])
+elapsed_zk <- sapply(times_zk, function(t) t[[3]])
+
+times_zk2 <- lapply(filenames, function(fn) {
+    print(paste0("Started file ", fn))
+    system.time(ad <- zellkonverter::readH5AD(fn))
+})
+
+user_zk2 <- sapply(times_zk2, function(t) t[[1]])
+elapsed_zk2 <- sapply(times_zk2, function(t) t[[3]])
+
+timings_noschard <- data.frame(
+    user = c(user_adR, user_zk, user_zk2),
+    elapsed = c(elapsed_adR, elapsed_zk, elapsed_zk2),
+    dataset = rep(rownames, 3),
+    package = c(rep("anndataR", 5), rep("zellkonverter (R)", 5), rep("zellkonverter (basilisk)", 5))
+)
+timings_noschard
+
+write.csv(timings_noschard, "runtime_benchmark/timings_noschard.csv")
+
+library(schard)
+
+times_schard <- lapply(filenames, function(fn) {
+    print(paste0("Started file ", fn))
+    system.time(sce <- schard::h5ad2sce(fn))
+})
+
+user_schard = sapply(times_schard, function(t) t[[1]])
+elapsed_schard = sapply(times_schard, function(t) t[[3]])
+
+schard_df <- data.frame(
+    user = user_schard,
+    elapsed = elapsed_schard,
+    dataset = rownames,
+    package = rep("schard", 5)
+)
+
+timings <- rbind(timings_noschard, schard_df)
+write.csv(timings, "runtime_benchmark/timings.csv")
+
+library(ggplot2)
+
+ggplot(
+    data = timings, 
+    aes(x = dataset, y = user, group = package, color = package)
+) + 
+geom_point() +
+geom_line()
+
+ggplot(
+    data = timings, 
+    aes(x = dataset, y = elapsed, group = package, color = package)
+) + 
+geom_point() +
+geom_line()
---
+++ runtime_benchmark/generate_datasets.py
@@ -0,0 +1,85 @@
+import anndata as ad
+import dummy_anndata as da
+import scipy as sp
+
+def generate_dataset(n_obs, n_vars, density=0.05):
+    x_mtx = sp.sparse.random(
+        n_obs,
+        n_vars,
+        density=density,
+        format="csr",
+        dtype="float32"
+    )
+
+    dataset = da.generate_dataset(
+        n_obs=n_obs,
+        n_vars=n_vars,
+        x_type = None,
+        layer_types = [],
+        obs_types = ["integer_array", "dense_array"],
+        var_types = ["integer_array", "dense_array"],
+        obsm_types = [],
+        varm_types = [],
+        obsp_types = [],
+        varp_types = [],
+        uns_types = [],
+        nested_uns_types = []
+    )
+    dataset.X = x_mtx
+    return dataset
+
+def generate_huge_sparse(n_obs, n_vars, density=0.05):
+    # split in 10 parts
+    n_parts = 10
+    n_obs_per_part = n_obs // n_parts
+
+    dataset = sp.sparse.random(n_obs_per_part, n_vars, density=density, format="csr", dtype="float32")
+
+    for i in range(n_parts - 1):
+        print(f"Generating part {i + 2} of {n_parts}")
+        part = sp.sparse.random(n_obs_per_part, n_vars, density=density, format="csr", dtype="float32")
+        dataset = sp.sparse.vstack([dataset, part], format="csc")
+        print(f"Part {i + 2} generated, current shape: {dataset.shape}")
+
+    return dataset
+
+d100 = generate_dataset(100, 20000)
+d100.write_h5ad("runtime_benchmark/data/d100.h5ad")
+
+print("Generated d100.h5ad")
+
+d1000 = generate_dataset(1000, 20000)
+d1000.write_h5ad("runtime_benchmark/data/d1000.h5ad")
+
+print("Generated d1000.h5ad")
+
+d10000 = generate_dataset(10000, 20000)
+d10000.write_h5ad("runtime_benchmark/data/d10000.h5ad")
+
+print("Generated d10000.h5ad")
+
+d100000 = generate_dataset(100000, 20000)
+d100000.write_h5ad("runtime_benchmark/data/d100000.h5ad")
+
+print("Generated d100000.h5ad")
+
+huge_x = generate_huge_sparse(500000, 20000)
+
+print("Generated huge sparse matrix with shape:")
+print(huge_x.shape)
+dataset = da.generate_dataset(
+    n_obs=500000,
+    n_vars=20000,
+    x_type=None,
+    layer_types=[],
+    obs_types=["integer_array", "dense_array"],
+    var_types=["integer_array", "dense_array"],
+    obsm_types=[],
+    varm_types=[],
+    obsp_types=[],
+    varp_types=[],
+    uns_types=[],
+    nested_uns_types=[]
+)
+dataset.X = huge_x
+dataset.write_h5ad("runtime_benchmark/data/d500000.h5ad")
